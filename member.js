55308cb953c0ebd5ac130e1b63c9f51f8f7fb7f1
sushmanr7 <36532446+sushmanr7@users.noreply.github.com>
2025-02-13 14:06:54 +0000

    commit on copilot multiple suggestions

---
+++ member.js
@@ -0,0 +1,13 @@
+function skillsMember() {
+  return (
+    <div className="skills-member">
+      <h2>Skills</h2>
+      <ul>
+        <li>JavaScript</li>
+        <li>React</li>
+        <li>Node.js</li>
+        <li>CSS</li>
+      </ul>
+    </div>
+  );
+}
